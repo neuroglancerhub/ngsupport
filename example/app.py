773730651290376a76258f3ff2095355589118a4
Stuart Berg <bergs@janelia.hhmi.org>
2020-04-09 14:48:41 -0400

    example: Try supporting different endpoints

--- example/app.py
+++ example/app.py
@@ -5,9 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/<string:endpoint>')
+def hello_world(endpoint=None):
     target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    return f'Hello {target} from ({endpoint})!\n'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
