c77795440ae05963fcb6b3c1b05b9160ce080ba0
Stuart Berg <bergs@janelia.hhmi.org>
2022-11-28 12:33:59 -0500

    Add /shortng url shortener, intended as a Slack bot

--- ngsupport/app.py
+++ ngsupport/app.py
@@ -1,18 +1,11 @@
-import os
 import logging
 
-import numpy as np
-
-from flask import Flask, Response, request, make_response
+from flask import Flask
 from flask_cors import CORS
-from requests import RequestException
 
 from neuclease import configure_default_logging
 configure_default_logging()
 logger = logging.getLogger(__name__)
-
-from .small_mesh import generate_and_store_mesh, gen_block_mesh
-from .locate_body import locate_body
 
 app = Flask(__name__)
 
@@ -20,16 +13,26 @@ app = Flask(__name__)
 CORS(app, origins=r'.*\.janelia\.org', supports_credentials=True)
 #CORS(app)
 
+
 @app.route('/small-mesh')
 def _small_mesh():
+    from ngsupport.small_mesh import generate_and_store_mesh
     return generate_and_store_mesh()
+
 
 @app.route('/block-mesh', methods=['POST'])
 def _block_mesh():
+    from ngsupport.small_mesh import gen_block_mesh
     return gen_block_mesh()
+
 
 @app.route('/locate-body')
 def _locate_body():
+    from ngsupport.locate_body import locate_body
     return locate_body()
 
 
+@app.route('/shortng', methods=['POST'])
+def _shortng():
+    from ngsupport.shortng import shortng
+    return shortng()
---
+++ ngsupport/shortng.py
@@ -0,0 +1,72 @@
+import os
+import datetime
+import tempfile
+import json
+import urllib
+
+from google.cloud import storage
+from flask import Response, request
+
+
+SHORTNG_BUCKET = 'flyem-user-links'  # Owned by FlyEM-Private
+
+
+def parse_nglink(link):
+    url_base, pseudo_json = link.split('#!')
+    pseudo_json = urllib.parse.unquote(pseudo_json)
+    data = json.loads(pseudo_json)
+    return url_base, data
+
+
+def upload_to_bucket(blob_name, blob_contents, bucket_name):
+    storage_client = storage.Client.from_service_account_json(
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(blob_contents)
+    return blob.public_url
+
+
+def shortng():
+    data = request.data.decode('utf-8').strip()
+    name_and_link = data.split(' ')
+    if len(name_and_link) == 0:
+        return Response("No link provided", 400)
+
+    if len(name_and_link) == 1 or name_and_link[0] == '{':
+        filename = datetime.datetime.now().strftime('%Y-%m-%d.%H%M%S')
+        link = data
+    else:
+        filename = name_and_link[0]
+        link = data[len(filename):].strip()
+
+    if not filename.endswith('.json'):
+        filename += '.json'
+
+    try:
+        state = json.loads(link)
+        url_base = 'https://clio-ng.janelia.org'
+    except ValueError:
+        try:
+            url_base, state = parse_nglink(link.strip())
+        except ValueError:
+            return Response(f"Could not parse link:\n\n{link}", 400)
+
+    if not (url_base.startswith('http://') or url_base.startswith('https://')):
+        return Response(f"Filename must not contain spaces, and links must start with http or https", 400)
+
+    # HACK:
+    # I store the *contents* of the credentials in the environment
+    # via the CloudRun settings, but the google API wants a filepath,
+    # not a JSON string.
+    # So I write the JSON text into a file before uploading.
+    _fd, creds_path = tempfile.mkstemp('.json')
+    with open(creds_path, 'w') as f:
+        f.write(os.environ['GOOGLE_APPLICATION_CREDENTIALS_CONTENTS'])
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = creds_path
+
+    state_string = json.dumps(state, indent=2)
+
+    upload_to_bucket(f'short/{filename}', state_string, SHORTNG_BUCKET)
+
+    return Response(f'{url_base}#!gs://{SHORTNG_BUCKET}/short/{filename}', 200)
