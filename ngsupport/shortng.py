b508f6f92104e697ac3e026c1f32d36c7104c06b
Stuart Berg <bergs@janelia.hhmi.org>
2022-11-29 12:28:30 -0500

    shortener: refactor and minor tweaks to UI

--- ngsupport/shortng.py
+++ ngsupport/shortng.py
@@ -11,30 +11,30 @@ from flask import Response, request, current_app, jsonify
 
 logger = logging.getLogger(__name__)
 
-
 SHORTNG_BUCKET = 'flyem-user-links'  # Owned by FlyEM-Private
+SHORTENER_URL = "https://shortng-bmcp5imp6q-uc.a.run.app/shortener.html"
 
 
-def parse_nglink(link):
-    url_base, pseudo_json = link.split('#!')
-    pseudo_json = urllib.parse.unquote(pseudo_json)
-    data = json.loads(pseudo_json)
-    return url_base, data
+class ErrMsg(RuntimeError):
+    def __init__(self, msg, from_slack):
+        self.msg = msg
+        self.from_slack = from_slack
+
+    def response(self):
+        if self.from_slack:
+            return jsonify({"text": self.msg, "response_type": "ephemeral"})
+        return Response(self.msg, 400)
 
 
-def upload_to_bucket(blob_name, blob_contents, bucket_name):
-    storage_client = storage.Client.from_service_account_json(
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-    blob.cache_control = 'public, no-store'
-    blob.upload_from_string(blob_contents, content_type='application/json')
-    return blob.public_url
+def shortener():
+    return current_app.send_static_file('shortener.html')
 
 
 def shortng():
     try:
         return _shortng()
+    except ErrMsg as ex:
+        return ex.response()
     except Exception as ex:
         logger.error(ex)
         raise
@@ -42,7 +42,50 @@ def shortng():
 
 def _shortng():
     """
+    Handle a request to shorten a neuroglancer link,
+    which might have come from one of three sources:
 
+    - Our web UI
+    - Our Slack bot (/shortng)
+    - A generic http request
+
+    In the case of the web UI, the filename, title, and
+    link text are specified in separate html form elements.
+
+    In the case of the Slack bot, the filename and link are
+    provided together, in the 'text' form element, and separated with a space.
+
+    In the case of a generic request (mostly for testing),
+    the link is in the body payload.
+
+    If no filename is provided, we construct a filename using a timestamp.
+    """
+    filename, title, link, from_slack, from_web = _parse_request()
+    url_base, state = _parse_state(link, from_slack)
+    if title:
+        state['title'] = title
+
+    bucket_path = _upload_state(state, filename)
+    url = f'{url_base}#!gs://{bucket_path}'
+    logger.info(f"Completed {url}")
+
+    if from_slack:
+        return jsonify({"text": url, "response_type": "ephemeral"})
+    elif from_web:
+        return _web_response(url, bucket_path)
+    else:
+        return Response(url, 200)
+
+
+def _parse_request():
+    """
+    Extract basic fields from the request and make
+    minor tweaks to them if needed (e.g. remove spaces).
+
+    Returns:
+        (filename, title, link, from_slack, from_web)
+        where from_slack means the Slackbot sent the request
+        and from_web means the web UI sent the request.
     """
     from_slack = ('Slackbot' in request.headers.get('User-Agent'))
     logger.info(f"from_slack: {from_slack}")
@@ -50,6 +93,7 @@ def _shortng():
     from_web = (request.form.get('client') == 'web')
     logger.info(f"from_web: {from_web}")
 
+    title = request.form.get('title', None)
     if 'text' in request.form:
         # https://api.slack.com/interactivity/slash-commands#app_command_handling
         data = request.form['text'].strip()
@@ -58,27 +102,18 @@ def _shortng():
         data = request.data.decode('utf-8').strip()
 
     data = data.replace('`', '')
-
-    title = request.form.get('title', None)
-
     if data == "" and from_slack:
         msg = (
             "No link provided. Use one of the following formats:\n"
             "```/shortng my-filename https://clio-ng.janelia.org/...```\n\n"
             "```/shortng https://clio-ng.janelia.org/...```\n\n"
             "Alternatively, try the web interface:\n"
-            "https://shortng-bmcp5imp6q-uc.a.run.app/shortener.html")
-        return jsonify({"text": msg, "response_type": "ephemeral"})
+            f"{SHORTENER_URL}")
+        raise ErrMsg(msg, from_slack)
 
-    logger.info(data)
     name_and_link = data.split(' ')
     if len(name_and_link) == 0:
-        msg = "Error: No link provided"
-        logger.error(msg)
-        if from_slack:
-            return jsonify({"text": msg, "response_type": "ephemeral"})
-        else:
-            return Response(msg, 400)
+        raise ErrMsg("Error: No link provided", from_slack)
 
     if len(name_and_link) == 1 or name_and_link[0] == '{':
         filename = request.form.get('filename', None)
@@ -95,30 +130,37 @@ def _shortng():
     # might be present if the user used the web UI.  Replace them.
     filename = filename.replace(' ', '_')
 
-    try:
-        state = json.loads(link)
-        url_base = 'https://clio-ng.janelia.org'
-    except ValueError:
-        try:
-            url_base, state = parse_nglink(link.strip())
-        except ValueError:
-            msg = f"Could not parse link:\n\n{link}"
-            logger.error(msg)
-            if from_slack:
-                return jsonify({"text": msg, "response_type": "ephemeral"})
-            return Response(msg, 400)
+    return filename, title, link, from_slack, from_web
 
-    if title:
-        state['title'] = title
+
+def _parse_state(link, from_slack):
+    """
+    Extract the neuroglancer state JSON data from the given link.
+    Raise ErrMsg if something went wrong.
+    """
+    try:
+        url_base, pseudo_json = link.split('#!')
+        pseudo_json = urllib.parse.unquote(pseudo_json)
+        state = json.loads(pseudo_json)
+    except ValueError:
+        if from_slack:
+            link = f"```{link}```"
+        msg = f"Could not parse link:\n\n{link}"
+        logger.error(msg)
+        raise ErrMsg(msg, from_slack)
 
     if not (url_base.startswith('http://') or url_base.startswith('https://')):
         msg = "Error: Filename must not contain spaces, and links must start with http or https"
         logger.error(msg)
-        if from_slack:
-            return jsonify({"text": msg, "response_type": "ephemeral"})
-        else:
-            return Response(msg, 400)
+        raise ErrMsg(msg, from_slack)
 
+    return url_base, state
+
+
+def _upload_state(state, filename):
+    """
+    Upload the given JSON state to a file in our (hard-coded) google storage bucket.
+    """
     # HACK:
     # I store the *contents* of the credentials in the environment
     # via the CloudRun settings, but the google API wants a filepath,
@@ -130,18 +172,31 @@ def _shortng():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = creds_path
 
     state_string = json.dumps(state, indent=2)
-    upload_to_bucket(f'short/{filename}', state_string, SHORTNG_BUCKET)
+    _upload_to_bucket(f'short/{filename}', state_string, SHORTNG_BUCKET)
 
-    url = f'{url_base}#!gs://{SHORTNG_BUCKET}/short/{filename}'
-    logger.info(f"Completed {url}")
+    bucket_path = f'{SHORTNG_BUCKET}/short/{filename}'
+    return bucket_path
 
-    if from_slack:
-        return jsonify({"text": url, "response_type": "ephemeral"})
 
-    if not from_web:
-        return Response(url, 200)
+def _upload_to_bucket(blob_name, blob_contents, bucket_name):
+    """
+    Upload a blob of data to the specified google storage bucket.
+    """
+    storage_client = storage.Client.from_service_account_json(
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.cache_control = 'public, no-store'
+    blob.upload_from_string(blob_contents, content_type='application/json')
+    return blob.public_url
 
-    download_url = f"https://storage.googleapis.com/{SHORTNG_BUCKET}/short/{filename}"
+
+def _web_response(url, bucket_path):
+    """
+    Return a little HTML page to display the shortened URL,
+    along with some convenient links.
+    """
+    download_url = f"https://storage.googleapis.com/{bucket_path}"
 
     script = dedent("""
         function copy_to_clipboard(text) {
@@ -169,12 +224,12 @@ def _shortng():
                 <img src=static/copy.jpg width=30 height=30>
             </a>
         </h2>
-        <h4><a href="" onclick="copy_to_clipboard('{url}'); return false;">[copy link]</a> <a href={download_url}>[view json]</a> <a href=shortener.html>[start over]</a><h4>
+        <h4>
+            <a href="" onclick="copy_to_clipboard('{url}'); return false;">[copy link]</a>
+            <a href={download_url}>[view json]</a>
+            <a href=shortener.html>[start over]</a>
+        </h4>
         </body>
         </html>
         """)
     return Response(page, 200)
-
-
-def shortener():
-    return current_app.send_static_file('shortener.html')
