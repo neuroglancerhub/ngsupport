2b4b9dbd1d08feaaaa638b8724f013f45d7109c2
Stuart Berg <bergs@janelia.hhmi.org>
2025-07-21 17:53:15 -0400

    synapse_annotations: Now supports point annotations, too.

--- ngsupport/synapse_annotations.py
+++ ngsupport/synapse_annotations.py
@@ -15,28 +15,29 @@ from .util import normalize_server
 logger = logging.getLogger(__name__)
 
 @normalize_server
-def synapse_annotations_info(server, uuid, instance):
-    info = _synapse_annotations_info(server, uuid, instance)
+def synapse_annotations_info(server, uuid, instance, annotation_type):
+    info = _synapse_annotations_info(server, uuid, instance, annotation_type)
     return jsonify(info), HTTPStatus.OK
 
 @normalize_server
-def synapse_annotations_by_id(server, uuid, instance, syn_id):
+def synapse_annotations_by_id(server, uuid, instance, annotation_type, syn_id):
     syn_id = int(syn_id)
-    return _synapse_annotations_by_id(server, uuid, instance, syn_id), HTTPStatus.OK
+    return _synapse_annotations_by_id(server, uuid, instance, annotation_type, syn_id), HTTPStatus.OK
 
 @normalize_server
-def synapse_annotations_by_related_id(server, uuid, instance, relationship, segment_id):
+def synapse_annotations_by_related_id(server, uuid, instance, annotation_type, relationship, segment_id):
     segment_id = int(segment_id)
-    return _synapse_annotations_by_related_id(server, uuid, instance, relationship, segment_id), HTTPStatus.OK
+    return _synapse_annotations_by_related_id(server, uuid, instance, annotation_type, relationship, segment_id), HTTPStatus.OK
 
 @cache
-def _synapse_annotations_info(server, uuid, syn_instance):
+def _synapse_annotations_info(server, uuid, syn_instance, annotation_type):
+    if annotation_type not in ['line', 'point']:
+        raise ValueError(f"Invalid annotation type: {annotation_type}")
+
     # Figure out which segmentation synapse instance is sync'd to
     # and determine the resolution.
     syn_info, seg_info = _get_instance_infos(server, uuid, syn_instance)
-
-    properties = json.loads((syn_info["Base"].get("Tags") or {}).get("neuroglancer-properties", '[]'))
-    relationships = json.loads((syn_info["Base"].get("Tags") or {}).get("neuroglancer-relationships", '[]'))
+    properties, relationships = _neuroglancer_properties_and_relationships(syn_info, annotation_type)
 
     seg_name = seg_info["Base"]["Name"]
     seg_units = seg_info["Extended"]["VoxelUnits"]
@@ -60,7 +61,7 @@ def _synapse_annotations_info(server, uuid, syn_instance):
         },
         "lower_bound": box_xyz[0].tolist(),
         "upper_bound": box_xyz[1].tolist(),
-        "annotation_type": "line",
+        "annotation_type": annotation_type,
         "properties": properties,
         "by_id": {
             "key": "by_id"
@@ -70,6 +71,53 @@ def _synapse_annotations_info(server, uuid, syn_instance):
     }
 
     return info
+
+
+def _neuroglancer_properties_and_relationships(syn_info, annotation_type):
+    # We assume the relationships/properties have been configured by default for line annotations, i.e. partners.
+    properties = json.loads((syn_info["Base"].get("Tags") or {}).get("neuroglancer-properties", '[]'))
+    relationships = json.loads((syn_info["Base"].get("Tags") or {}).get("neuroglancer-relationships", '[]'))
+
+    # If the user is only showing the points on each neuron itself (no partners),
+    # then performance will be much better.
+    # First we need to filter/convert the properties for use with point annotations.
+    if annotation_type == 'line':
+        return properties, relationships
+
+    # Reduce the pairs of {prop}_pre, {prop}_post to {prop}
+    pre_props = {p['id'][:-len('_pre')]: p for p in properties if p['id'].endswith('_pre')}
+    post_props = {p['id'][:-len('_post')]: p for p in properties if p['id'].endswith('_post')}
+    usable_props = set(pre_props) & set(post_props)
+
+    properties = []
+    for prop_name in usable_props:
+        p = pre_props[prop_name]
+        p['id'] = prop_name
+        properties.append(p)
+
+    # Append a 'kind' property for the point annotations.
+    # Note: It's okay to put it on the end, since it just so happens
+    # that neuroglancer uint8 properties are supposed to come last.
+    properties.append({
+        'id': 'kind',
+        'type': 'uint8',
+        'enum_values': [0, 1],
+        'enum_labels': ['PreSyn', 'PostSyn']
+    })
+
+    # Reduce the pairs of {rel}_pre, {rel}_post to {rel}
+    pre_rels = {r['id'][:-len('_pre')]: r for r in relationships if r['id'].endswith('_pre')}
+    post_rels = {r['id'][:-len('_post')]: r for r in relationships if r['id'].endswith('_post')}
+    usable_rels = set(pre_rels) & set(post_rels)
+    
+    relationships = []
+    for rel_name in usable_rels:
+        r = pre_rels[rel_name]
+        r['id'] = rel_name
+        r['key'] = f"by_rel_{rel_name}"
+        relationships.append(r)
+
+    return properties, relationships
 
 
 @cache
@@ -87,25 +135,36 @@ def _get_instance_infos(server, uuid, syn_instance):
     return syn_info, seg_info
 
 
-def _synapse_annotations_by_id(server, uuid, instance, syn_id):
+def _synapse_annotations_by_id(server, uuid, instance, annotation_type, syn_id):
     from neuroglancer.coordinate_space import CoordinateSpace
     from neuclease.util import decode_coords_from_uint64
     from neuclease.dvid.annotation import fetch_elements
+    from neuclease.dvid.labelmap import fetch_label
     from neuclease.misc.neuroglancer.annotations.precomputed import write_precomputed_annotations
 
-    info = _synapse_annotations_info(server, uuid, instance)
+    info = _synapse_annotations_info(server, uuid, instance, annotation_type)
 
     syn_ids = np.array([syn_id], np.uint64)
     zyx = decode_coords_from_uint64(syn_ids)[0]
-    syn_df, rel_df = fetch_elements(server, uuid, instance, [zyx, zyx+1], relationships=True, format='pandas')
-    partner_df = _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info)
-    assert len(partner_df) == 1, "Expected exactly one partner"
+
+    if annotation_type == 'line':
+        syn_df, rel_df = fetch_elements(server, uuid, instance, [zyx, zyx+1], relationships=True, format='pandas')
+        ann_df = _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info)
+        assert len(ann_df) == 1, "Expected exactly one partner"
+    else:
+        ann_df = fetch_elements(server, uuid, instance, [zyx, zyx+1], relationships=False, format='pandas')
+        ann_df.index = [syn_id]
+
+        syn_info, seg_info = _get_instance_infos(server, uuid, instance)
+        seg_name = seg_info["Base"]["Name"]
+        ann_df['body'] = fetch_label(server, uuid, seg_name, zyx)
+        ann_df = _convert_from_strings(ann_df, info)
 
     with tempfile.TemporaryDirectory() as tmpdir:
         write_precomputed_annotations(
-            partner_df,
+            ann_df,
             coord_space=CoordinateSpace(json=info['dimensions']),
-            annotation_type='line',
+            annotation_type=annotation_type,
             properties=info['properties'],
             relationships=[r['id'] for r in info['relationships']],
             output_dir=tmpdir,
@@ -118,23 +177,32 @@ def _synapse_annotations_by_id(server, uuid, instance, syn_id):
         return open(f'{tmpdir}/by_id/{syn_id}', 'rb').read()
 
 
-def _synapse_annotations_by_related_id(server, uuid, instance, relationship, segment_id):
+def _synapse_annotations_by_related_id(server, uuid, instance, annotation_type, relationship, segment_id):
     from neuroglancer.coordinate_space import CoordinateSpace
+    from neuclease.util import encode_coords_to_uint64
     from neuclease.dvid.annotation import fetch_label
     from neuclease.misc.neuroglancer.annotations.precomputed import write_precomputed_annotations
 
-    info = _synapse_annotations_info(server, uuid, instance)
+    info = _synapse_annotations_info(server, uuid, instance, annotation_type)
 
     rel_dict = {r['id']: r for r in info['relationships']}
     rel_id = rel_dict[relationship]['id']
-    syn_df, rel_df = fetch_label(server, uuid, instance, segment_id, relationships=True, format='pandas')
-    partner_df = _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info)
+
+    if annotation_type == 'line':
+        syn_df, partner_df = fetch_label(server, uuid, instance, segment_id, relationships=True, format='pandas')
+        syn_df['body'] = segment_id
+        ann_df = _fetch_partner_properties(server, uuid, instance, syn_df, partner_df, info)
+    else:
+        syn_df = fetch_label(server, uuid, instance, segment_id, relationships=False, format='pandas')
+        syn_df.index = encode_coords_to_uint64(syn_df[[*'zyx']].values)
+        syn_df['body'] = segment_id
+        ann_df = _convert_from_strings(syn_df, info)
 
     with tempfile.TemporaryDirectory() as tmpdir:
         write_precomputed_annotations(
-            partner_df,
+            ann_df,
             coord_space=CoordinateSpace(json=info['dimensions']),
-            annotation_type='line',
+            annotation_type=annotation_type,
             properties=info['properties'],
             relationships=[r['id'] for r in info['relationships']],
             output_dir=tmpdir,
@@ -146,15 +214,16 @@ def _synapse_annotations_by_related_id(server, uuid, instance, relationship, seg
 
         return open(f'{tmpdir}/by_rel_{rel_id}/{segment_id}', 'rb').read()
 
-def _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info):
+
+def _fetch_partner_properties(server, uuid, instance, syn_df, partner_df, info):
     from neuclease.util import swap_df_cols, encode_coords_to_uint64
     from neuclease.dvid.annotation import fetch_label, fetch_point_elements_by_block
     from neuclease.dvid.labelmap import fetch_labels_batched
 
     syn_df = syn_df.drop(columns=['tags'])
-    rel_df = rel_df.drop(columns=['rel'])
+    partner_df = partner_df.drop(columns=['rel'])
 
-    partner_df = syn_df.merge(rel_df, 'left', on=[*'xyz']).rename(columns={c: f'from_{c}' for c in syn_df.columns})
+    partner_df = syn_df.merge(partner_df, 'left', on=[*'xyz']).rename(columns={c: f'from_{c}' for c in syn_df.columns})
     partner_df = fetch_point_elements_by_block(
         server, uuid, instance,
         partner_df.rename(columns={f'to_{k}': k for k in 'xyz'})
@@ -195,7 +264,12 @@ def _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info):
     partner_df = partner_df.rename(columns={f'{k}_post': f'{k}b' for k in 'xyz'})
 
     partner_df = partner_df.drop(columns=['kind_pre', 'kind_post'])
+    partner_df = _convert_from_strings(partner_df, info)    
 
+    return partner_df
+
+
+def _convert_from_strings(df, info):
     # Properties from DVID are stored as strings.
     # We'll convert them to the appropriate type.
     dtypes = {}
@@ -213,7 +287,4 @@ def _fetch_partner_properties(server, uuid, instance, syn_df, rel_df, info):
         # We only support relationships with single-valued IDs, not lists of IDs, and no nulls.
         dtypes[r['id']] = np.uint64
 
-    partner_df = partner_df.astype(dtypes)
-
-    return partner_df
-
+    return df.astype(dtypes)
